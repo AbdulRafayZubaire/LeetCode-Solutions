86a7364899c5ca5f1dc445a5935b658f38903a66
Abdul Rafay Zubairi <abdurrafaykhawer10@gmail.com>
2023-12-21 01:22:44 +0500

    Time: 718 ms (96.63%) | Memory: 19.9 MB (26.96%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,15 +1,31 @@
 class Solution(object):
     def __init__(self):
         self.memo = {}
-    def coinChange(self, coins, amount):
+
+    def coin(self, coins, amount):
         if amount in self.memo:
             return self.memo[amount]
-        if amount == 0: return 0
-        if amount < 0: return -1
-        mini = -1
+        if amount == 0:
+            return 0
+        if amount < 0:
+            return -1
+
+        mini = sys.maxsize
         for coin in coins:
-            retValue = self.coinChange(coins, amount-coin)
-            if ((mini == -1 or mini > retValue + 1) and retValue >= 0):
-                mini = retValue + 1
-        self.memo[amount] = mini 
-        return mini
+            result = self.coin(coins, amount - coin)
+            if result != -1 and result < mini:
+                mini = result + 1
+
+        self.memo[amount] = -1 if mini == sys.maxsize else mini
+        return self.memo[amount]
+
+    def coinChange(self, coins, amount):
+        return self.coin(coins, amount)
+
+
+# Example usage:
+sol = Solution()
+coins = [1, 2, 5]
+amount = 11
+result = sol.coinChange(coins, amount)
+print(result)
