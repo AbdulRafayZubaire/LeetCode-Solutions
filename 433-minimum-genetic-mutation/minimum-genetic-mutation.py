914f6efc783f4818c7c1407ffc7ded4c5a387874
Abdul Rafay Zubairi <abdurrafaykhawer10@gmail.com>
2024-03-06 14:48:03 +0500

    Time: 27 ms (93.86%) | Memory: 16.5 MB (64.04%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.py
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -5,7 +5,6 @@ class Solution:
             return -1
 
         result = []
-        visited = set()
 
         def diff(curr, next):
             count = sum(x != y for x, y in zip(curr, next))
@@ -23,5 +22,7 @@ class Solution:
                     bank_copy.remove(next)
                     bfs(next, bank_copy, count + 1)
             return result
+
         bfs(startGene, bank, 0)
+        
         return min(result) if len(result) > 0 else -1
